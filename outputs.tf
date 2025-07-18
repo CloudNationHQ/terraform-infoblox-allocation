20bffab569108339663e7ad5d19ee7531cf05e23
dkooll <dennis.kool@cloudnation.nl>
2025-07-18 15:39:45 +0200

    feat: small refactor

--- outputs.tf
+++ outputs.tf
@@ -31,17 +31,17 @@ output "debug_subnets" {
 output "subnet_cidrs" {
   description = "Map of subnet CIDRs"
   value = {
-    for key, subnet in infoblox_ipv4_network.subnets : key => subnet.cidr
+    for key, subnet_config in var.subnets : key => infoblox_ipv4_network.subnets[key].cidr
   }
 }
 
 output "subnet_details" {
   description = "Complete subnet details"
   value = {
-    for key, subnet in infoblox_ipv4_network.subnets : key => {
-      cidr         = subnet.cidr
-      network_view = subnet.network_view
-      comment      = subnet.comment
+    for key, subnet_config in var.subnets : key => {
+      cidr         = infoblox_ipv4_network.subnets[key].cidr
+      network_view = infoblox_ipv4_network.subnets[key].network_view
+      comment      = infoblox_ipv4_network.subnets[key].comment
       parent_cidr  = infoblox_ipv4_network_container.network.cidr
     }
   }
